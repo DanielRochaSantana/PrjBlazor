87c4ee8bbdc57a80c75ca101ff8419cbee86c50d
Daniel Rocha Santana <danibas92@hotmail.com>
2025-08-13 19:16:42 -0300

    Adições: projetos e arquivos adicionados.

---
+++ Backend.Domain/Models/Entity/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Domain.Models.Base;
+
+namespace Backend.Domain.Models.Entity
+{
+    public class Usuario : BaseEntity
+    {
+        /// <summary>
+        /// A propriedade Nome.
+        /// </summary>
+        public string Nome { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A propriedade Email.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A propriedade Celular.
+        /// </summary>
+        public string Celular { get; set; } = string.Empty;
+    }
+}
