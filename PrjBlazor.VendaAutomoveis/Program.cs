752e32f8109a19b9a6ea53fad3b6e5e4bfa30096
Daniel Rocha Santana <danibas92@hotmail.com>
2025-09-04 17:33:38 -0300

    Implementações: implementações efetuadas.

--- PrjBlazor.VendaAutomoveis/Program.cs
+++ PrjBlazor.VendaAutomoveis/Program.cs
@@ -1,14 +1,26 @@
+using Backend.Application.Interfaces;
+using Backend.Application.Services;
+using Backend.Domain.Models.Entity;
+using Backend.Infrastructure.CommandSide;
+using Backend.Infrastructure.Context;
+using Backend.Infrastructure.Interfaces.CommandSide;
+using Backend.Infrastructure.Interfaces.Context;
+using Backend.Infrastructure.Interfaces.QuerySide;
+using Backend.Infrastructure.QuerySide;
 using PrjBlazor.VendaAutomoveis.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IRepositorioGenerico<Usuario>, RepositorioGenerico<Usuario>>();
+builder.Services.AddScoped<IConsultaGenerica<Usuario>, ConsultaGenerica<Usuario>>();
+builder.Services.AddScoped<IContext, Context>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
